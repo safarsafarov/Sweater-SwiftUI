70cea25223e59f55bfdd3f6575bf2aa8db1529d7
Safar Safarov <Safarov.safar@hotmail.com>
2022-09-02 03:09:25 +0500

    ✨

---
+++ Shoppo-SwiftUI/Shared/Shoppo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoppo_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Safar Safarov on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Shoppo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
